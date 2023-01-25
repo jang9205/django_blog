c41a8553ec73525cdb02c0ebf67c00e5d808ded7
jang9205 <wer4886@naver.com>
2023-01-26 01:44:45 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,13 @@ def index(request):
                   {
                       'posts':posts,
                   })
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' : post,
+        }
+    )
