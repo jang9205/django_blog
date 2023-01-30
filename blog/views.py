4ffff88ad9cb74db0b2f53ed869855ca4f00d4db
jang9205 <wer4886@naver.com>
2023-01-30 15:20:50 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-from . models import Post, Category
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from .models import Post, Category
 
 class PostList(ListView):
     model = Post
@@ -26,6 +26,24 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'category' : category,
+        }
+    )
 # def single_post_page(request, pk):
 #     post = Post.objects.get(pk=pk)
 #     return render(
