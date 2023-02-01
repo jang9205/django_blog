7492acb4e21a1af9eb655c3fc0a9442221cf0b48
jang9205 <wer4886@naver.com>
2023-02-02 02:04:26 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -62,3 +62,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1440/698ca50f2a9f3b45/svg/{self.author.email}'
